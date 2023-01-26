3cd4865e1040a2f48efe2bf33cff6d6fc0754332
Matt Zachman <mzachman14@gmail.com>
2023-01-25 22:35:16 -0500

    Write skeleton for Work component

---
+++ src/components/Work.js
@@ -0,0 +1,26 @@
+// Work.js
+
+import React, { Component } from "react";
+
+
+class Work extends Component {
+  constructor() {
+    super();
+
+
+    
+  }
+  
+  render() {
+    
+
+
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+export default Work;
