27f5dacac875172d5ebe08d77c002d8d68039e97
Matt Zachman <mzachman14@gmail.com>
2023-01-25 22:37:55 -0500

    Import PEW components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Personal from "./components/Personal";
+import Education from "./components/Education";
+import Work from "./components/Work";
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+
+    
+  }
+  
+  render() {
+    
+
+
+    return (
+      <div>
+        <Personal />
+        <Education />
+        <Work />
+      </div>
+    );
+  }
 }
 
 export default App;
