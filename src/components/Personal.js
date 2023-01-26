801b87b0520e7179c0290bf9c8abef37dfb17467
Matt Zachman <mzachman14@gmail.com>
2023-01-25 22:34:58 -0500

    Write skeleton for Personal component

---
+++ src/components/Personal.js
@@ -0,0 +1,26 @@
+// Personal.js
+
+import React, { Component } from "react";
+
+
+class Personal extends Component {
+  constructor() {
+    super();
+
+
+    
+  }
+  
+  render() {
+    
+
+
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+export default Personal;
