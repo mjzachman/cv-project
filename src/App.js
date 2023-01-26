08b8d6e41f10665cfe039d80d9eaada7c49ecebe
Matt Zachman <mzachman14@gmail.com>
2023-01-25 23:18:17 -0500

    Write elements and styling to Personal component

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 
 
     return (
-      <div>
+      <div className="App">
         <Personal />
         <Education />
         <Work />
--- src/components/Personal.js
+++ src/components/Personal.js
@@ -17,7 +17,56 @@ class Personal extends Component {
 
     return (
       <div className="section">
-        <h1>Personal</h1>
+        <h2>Personal Information</h2>
+        <form>
+        <fieldset>
+            <div class="input-div">
+                <label htmlFor="nameInput">First Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                />
+            </div>
+            <div class="input-div">
+                <label htmlFor="emailInput">Middle Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                />
+            </div>
+            <div class="input-div">
+                <label htmlFor="phoneInput">Last Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                />
+            </div>
+            <div class="input-div">
+                <label htmlFor="nameInput">Age:</label>
+                <input
+                  type="number"
+                  id="age"
+                />
+            </div>
+            <div class="input-div">
+                <label htmlFor="emailInput">Email Address:</label>
+                <input
+                  type="email"
+                  id="email"
+                />
+            </div>
+            <div class="input-div">
+                <label htmlFor="phoneInput">Phone Number:</label>
+                <input
+                  type="tel"
+                  id="phone"
+                />
+            </div>
+        </fieldset>
+        <button type="submit">
+          Submit
+        </button>
+      </form>
       </div>
     );
   }
