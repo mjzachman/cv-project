59e7c79b7acc0ffc1653de11650034a715cac754
Matt Zachman <mzachman14@gmail.com>
2023-01-25 22:34:28 -0500

    Write skeleton for Education component

---
+++ src/components/Education.js
@@ -0,0 +1,26 @@
+// Education.js
+
+import React, { Component } from "react";
+
+
+class Education extends Component {
+  constructor() {
+    super();
+
+
+    
+  }
+  
+  render() {
+    
+
+
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+export default Education;
