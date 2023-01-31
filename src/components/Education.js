0865271abc00639fadc4075a5c00306364a2ce48
Matt Zachman <mzachman14@gmail.com>
2023-01-31 13:13:12 -0500

    Write form logic/elements to education and work components

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,40 +4,77 @@ import InputDiv from "./InputDiv";
 class Education extends Component {
   constructor() {
     super();
+    this.state = {
+      inst: '',
+      area: '',
+      level: '',
+      date: ''
+    };
 
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
     
   }
   
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
   render() {
     
 
 
     return (
       <div className="section">
-        <h2>Education</h2>
-        <form>
-        <fieldset className="short-form">
-            <InputDiv 
-              id="eduInst"
-              type="text"
-              text="Institution Name: "
-              />
-            <InputDiv 
-              id="eduStudy"
-              type="text"
-              text="Area of Study: "
-              />
-            <InputDiv 
-              id="eduLevel"
-              type="text"
-              text="Level of Education Completed: "
-              />
-            <InputDiv 
-              id="eduDate"
-              type="date"
-              text="Date Completed: "
-              />
+      <form onSubmit={this.handleSubmit}>
+        <fieldset className = "short-form">
+          <div className="input-div">
+            <label htmlFor='inst'>Academic Institution: </label>
+            <input
+            type= 'text'
+            name= 'inst'
+            value= {this.state.inst}
+            onChange = {this.handleInputChange}
+            />
+          </div>
+          <div className="input-div">
+            <label htmlFor='area'>Area of Study: </label>
+            <input
+            type= 'text'
+            name= 'area'
+            value= {this.state.area}
+            onChange = {this.handleInputChange}
+            />
+          </div>
+          <div className="input-div">
+            <label htmlFor='level'>Level of Education Completed: </label>
+            <input
+            type= 'text'
+            name= 'level'
+            value= {this.state.level}
+            onChange = {this.handleInputChange}
+            />
+          </div>
+          <div className="input-div">
+            <label htmlFor='date'>Completeion date: </label>
+            <input
+            type= 'date'
+            name= 'date'
+            value= {this.state.date}
+            onChange = {this.handleInputChange}
+            />
+          </div>
         </fieldset>
         <button type="submit">
           Submit
--- src/components/Work.js
+++ src/components/Work.js
@@ -4,11 +4,31 @@ import InputDiv from "./InputDiv";
 class Work extends Component {
   constructor() {
     super();
+    this.state = {
+      job: '',
+      company: '',
+      start: '',
+      end: ''
+    };
 
-
-    
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
   render() {
     
 
@@ -16,28 +36,44 @@ class Work extends Component {
     return (
       <div className="section">
         <h2>Work History</h2>
-        <form>
-        <fieldset className="short-form">
-            <InputDiv 
-              id="jobTitle"
-              type="text"
-              text="Job Title: "
-              />
-            <InputDiv 
-              id="compName"
-              type="text"
-              text="Company Name: "
-              />
-            <InputDiv 
-              id="startDate"
-              type="date"
-              text="Start Date: "
-              />
-            <InputDiv 
-              id="endDate"
-              type="date"
-              text="End Date: "
-              />
+        <form onSubmit={this.handleSubmit}>
+        <fieldset className = "short-form">
+          <div className="input-div">
+            <label htmlFor='job'>Job Title: </label>
+            <input
+            type= 'text'
+            name= 'job'
+            value= {this.state.job}
+            onChange = {this.handleInputChange}
+            />
+          </div>
+          <div className="input-div">
+            <label htmlFor='company'>Company: </label>
+            <input
+            type= 'text'
+            name= 'company'
+            value= {this.state.company}
+            onChange = {this.handleInputChange}
+            />
+          </div>
+          <div className="input-div">
+            <label htmlFor='start'>Start Date: </label>
+            <input
+            type= 'date'
+            name= 'start'
+            value= {this.state.start}
+            onChange = {this.handleInputChange}
+            />
+          </div>
+          <div className="input-div">
+            <label htmlFor='date'>End Date: </label>
+            <input
+            type= 'date'
+            name= 'end'
+            value= {this.state.end}
+            onChange = {this.handleInputChange}
+            />
+          </div>
         </fieldset>
         <button type="submit">
           Submit
